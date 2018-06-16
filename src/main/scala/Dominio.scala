ef8559f54a5ecd440a04c1d1730f73b0624dc758
Juanjo Sierra <jjsierra103@gmail.com>
2018-06-16 19:42:23 +0200

    Añadido mapa de pesos para las variables del dominio

--- src/main/scala/Dominio.scala
+++ src/main/scala/Dominio.scala
@@ -4,4 +4,33 @@ class Dominio (val variables : List[Variable]) {
     * Dato miembro que almacena pares variable - índice en un mapa
     */
   val indiceVariables = variables.zipWithIndex.toMap
+
+  /**
+    * Función privada para calcular pesos de un dominio
+    */
+  private def calcularPesos : List[Int] = {
+
+    var listaPesos = List[Int]()
+
+    def go(indice : Int): Int = {
+      if (indice == variables.length-1) {
+        listaPesos = 1 :: listaPesos
+        return 1
+      }
+      else {
+        val peso = variables(indice+1).numEstados * go(indice+1)
+        listaPesos = peso :: listaPesos
+        return peso
+      }
+    }
+
+    go(0)
+
+    return listaPesos
+  }
+
+  /**
+    * Dato miembro que almacena pares variable - peso en un mapa
+    */
+  val pesosVariables = (variables zip calcularPesos).toMap
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,13 @@
 object Main extends App {
 
-  val x1 = Variable("X1", 3)
-  val x2 = Variable("X2", 2)
-  val x3 = Variable("X3", 10)
+  val x1 = Variable("X1", 5)
+  val x2 = Variable("X2", 4)
+  val x3 = Variable("X3", 3)
 
   val dom = new Dominio(List(x1, x2, x3))
   val indice = dom.indiceVariables(x3)
   println(indice)
+
+  val pesos = dom.pesosVariables
+  println(pesos)
 }
