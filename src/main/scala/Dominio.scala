dd6b33e0fbfcd5c5127b3f24a6ac30f4ff396a36
Juanjo Sierra <jjsierra103@gmail.com>
2018-06-16 19:01:29 +0200

    Creada clase Dominio, generado primer dato miembro

--- src/main/scala/Dominio.scala
+++ src/main/scala/Dominio.scala
@@ -1,1 +1,7 @@
+class Dominio (val variables : List[Variable]) {
 
+  /**
+    * Dato miembro que almacena pares variable - índice en un mapa
+    */
+  val indiceVariables = variables.zipWithIndex.toMap
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,10 @@
-import Variable._
-
 object Main extends App {
 
-  val x1 = new Variable("X1", 3)
+  val x1 = Variable("X1", 3)
   val x2 = Variable("X2", 2)
+  val x3 = Variable("X3", 10)
 
-  println(x1)
-  println(x2)
+  val dom = new Dominio(List(x1, x2, x3))
+  val indice = dom.indiceVariables(x3)
+  println(indice)
 }
